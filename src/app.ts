347e041e919e210d973f6f53d54db97968fa60a6
Pavleo Tkachik <theendermanmaps@gmail.com>
2024-05-20 23:31:44 +0300

    Lab6AllCom

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import userRouter from './routes/user.routes'
+import postRouter from './routes/post.routes'
+import commentRouter from './routes/comment.routes'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!')
+    return
+  }
+  next()
+})
+
+app.use('/users', userRouter)
+app.use('/posts', postRouter)
+app.use('/comments', commentRouter)
+
+export default app
